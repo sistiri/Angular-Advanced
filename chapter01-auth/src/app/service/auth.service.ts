153f3c26f6b3d57748ea3446589e4b4cd51764e6
sistiri <sistiri@yahoo.com>
2021-07-28 00:15:20 +0200

    04 - AuthService - Login - accessToken, localStorage

--- chapter01-auth/src/app/service/auth.service.ts
+++ chapter01-auth/src/app/service/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ConfigService } from './config.service';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
 import { User } from '../model/user';
 import { Router } from '@angular/router';
 import { UserService } from './user.service';
@@ -11,10 +12,11 @@ import { UserService } from './user.service';
 })
 export class AuthService {
 
-  loginUrl = `${this.config.apiUrl}login`
-  logoutUrl = `${this.config.apiUrl}logout`
-  currentUserSubject: BehaviorSubject<User | null> = new BehaviorSubject<User |null>(null)
-  lastToken: string | null = null
+  loginUrl = `${this.config.apiUrl}login`;
+  logoutUrl = `${this.config.apiUrl}logout`;
+  storageName = 'currentUser';
+  currentUserSubject: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
+  lastToken: string = '';
 
   constructor(
     private config: ConfigService,
@@ -27,9 +29,38 @@ export class AuthService {
     return this.currentUserSubject.value
   }
 
-  logout() {
-    localStorage.removeItem('currentUser')
-    this.currentUserSubject.next(null)
-    this.router.navigate(['login'])
+  // login(loginData: User): Observable<{ accessToken: string }> {
+    login(loginData: User):Observable<User | User[] | null> {
+      return this.http.post<{ accessToken: string }>(
+      this.loginUrl,
+      { email: loginData.email, password: loginData.password }
+    )
+      .pipe(switchMap(response => {
+        if (response.accessToken) {
+          this.lastToken = response.accessToken;
+          return this.userService.query(`email=${loginData.email}`);
+        }
+        return of(null);
+      }))
+      .pipe(
+        tap(user => {
+          if (!user) {
+            localStorage.removeItem(this.storageName);
+            this.currentUserSubject.next(null);
+          } else {
+            // user[0].token = this.lastToken;
+            (user as User[])[0].token = this.lastToken;
+            localStorage.setItem(this.storageName, JSON.stringify(user as User[])[0]);
+            this.currentUserSubject.next((user as User[])[0]);
+          }
+        })
+      );
   }
+
+  logout() {
+    localStorage.removeItem(this.storageName);
+    this.currentUserSubject.next(null);
+    this.router.navigate(['login']);
+  }
+
 }
